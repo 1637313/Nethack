dc530e0bfb6dd670f1e6b0525bd3caecc0c0af75
Londani Zuma <47749080+1637313@users.noreply.github.com>
2020-11-12 20:19:35 +0200

    removed embedding layer

--- Nethack.py
+++ Nethack.py
@@ -90,7 +90,26 @@ class ReplayBuffer:
         indices = np.random.randint(0, len(self._storage) - 1, size=batch_size)
         return self._encode_sample(indices)
 
+def to_state(state):
+    glyphs_matrix = state['glyphs']
+    agent_stat = state['blstats']
+    agent_row = state['blstats'][1]
+    agent_col = state['blstats'][0]
 
+    around_agent = np.zeros([9,9])
+    row = agent_row - 4
+    col = agent_col -4
+    for i in range(9):
+        for j in range(9):
+            if row>0 and row<glyphs_matrix.shape[0] and col>0 and col<glyphs_matrix.shape[1]:
+                around_agent[i][j] = glyphs_matrix[row][col]
+            col+=1
+        col = agent_col -4
+        row+=1
+        
+    return glyphs_matrix.copy(),around_agent.copy(),agent_stat.copy()
+    
+    
 class AbstractAgent:
     """
     AbstractAgent
@@ -118,31 +137,16 @@ class MyAgent(AbstractAgent):
 
         # TODO: return selected action
         
-        glyphs_matrix = observation['glyphs']
-        agent_stat = observation['blstats']
-        agent_row = observation['blstats'][1]
-        agent_col = observation['blstats'][0]
-
-        around_agent = np.zeros([9,9])
-        row = agent_row - 4
-        col = agent_col -4
-        for i in range(9):
-            for j in range(9):
-                if row>0 and row<glyphs_matrix.shape[0] and col>0 and col<glyphs_matrix.shape[1]:
-                    around_agent[i][j] = glyphs_matrix[row][col]
-                col+=1
-            col = agent_col -4
-            row+=1
-            
-        glyphs_matrix = torch.from_numpy(glyphs_matrix).type(torch.LongTensor).to(device).unsqueeze(0)
-        agent_stat = torch.from_numpy(agent_stat).to(device).unsqueeze(0)
-        around_agent = torch.from_numpy(around_agent).type(torch.LongTensor).to(device).unsqueeze(0)
+        glyphs_matrix,around_agent,agent_stat = to_state(observation)
+        glyphs_matrix = torch.from_numpy(glyphs_matrix).type(torch.FloatTensor).to(device).unsqueeze(0).unsqueeze(0)/5991.0
+        agent_stat = torch.from_numpy(agent_stat).type(torch.FloatTensor).to(device).unsqueeze(0)
+        around_agent = torch.from_numpy(around_agent).type(torch.FloatTensor).to(device).unsqueeze(0).unsqueeze(0)/5991.0
         state = (glyphs_matrix,around_agent,agent_stat)
 
         with torch.no_grad():
             outputs = self.model(state)
             action = torch.argmax(outputs)
-        return action_to_mean(action.item())
+        return action.item()
 
 class RandomAgent(AbstractAgent):
     def __init__(self, observation_space, action_space):
@@ -156,13 +160,13 @@ class RandomAgent(AbstractAgent):
 def run_episode(env):
     # create instance of MyAgent
     # from MyAgent import MyAgent
-    agent = MyAgent(env.observation_space, 19)
+    agent = MyAgent(env.observation_space,env.action_space.n)
 
     done = False
     episode_return = 0.0
     state = env.reset()
 
-    save_dir = './animations/'
+    save_dir = './animations_nethack_dqn/'
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
@@ -192,10 +196,8 @@ class DQN(nn.Module):
         :param action_space: the action space of the environment
         """
         super(DQN,self).__init__()
-        self.embed1 = nn.Embedding(5991,32)
-        
         self.glyph_model = nn.Sequential(
-                  nn.Conv2d(32, 16, kernel_size=3, stride=1,padding = 1),
+                  nn.Conv2d(1, 16, kernel_size=3, stride=1,padding = 1),
                   nn.ReLU(),
                   nn.Conv2d(16, 16, kernel_size=3, stride=1,padding = 1),
                   nn.ReLU(),
@@ -208,7 +210,7 @@ class DQN(nn.Module):
                 )
 
         self.around_agent_model = nn.Sequential(
-                  nn.Conv2d(32, 16, kernel_size=3, stride=1,padding = 1),
+                  nn.Conv2d(1, 16, kernel_size=3, stride=1,padding = 1),
                   nn.ReLU(),
                   nn.Conv2d(16, 16, kernel_size=3, stride=1,padding = 1),
                   nn.ReLU(),
@@ -234,22 +236,15 @@ class DQN(nn.Module):
                 )
         self.outputLayer = nn.Linear(128,action_space)
 
-    def _select(self,embed,x):
-        output = embed.weight.index_select(0,x.reshape(-1))
-        return output.reshape(x.shape+(-1,))
     
     def forward(self, state):
-        x = self._select(self.embed1,state[0])
-        x = x.permute(0,3,1,2).contiguous()
-        x = self.glyph_model(x)
+        x = self.glyph_model(state[0])
         x = torch.reshape(x,(x.size(0),-1))
         
-        y = self._select(self.embed1,state[1])
-        y = y.permute(0,3,1,2).contiguous()
-        y = self.around_agent_model(y)
+        y = self.around_agent_model(state[1])
         y = torch.reshape(y,(y.size(0),-1))
         
-        z = self.agent_stats_mlp(state[2].float())
+        z = self.agent_stats_mlp(state[2])
         z = torch.reshape(z,(z.size(0),-1))
         
         o = torch.cat((x, y, z), 1)
@@ -294,16 +289,16 @@ class DQNAgent:
         rewards = torch.from_numpy(np.array(rewards)).to(device)
         dones = torch.from_numpy(np.array(dones)).to(device)
         
-        matrixs = torch.from_numpy(matrixs).type(torch.LongTensor).to(device)
-        agent_stats = torch.from_numpy(agent_stats).to(device)
-        around_agents = torch.from_numpy(around_agents).type(torch.LongTensor).to(device)
+        matrixs = torch.from_numpy(matrixs).type(torch.FloatTensor).to(device).unsqueeze(1)/5991.0
+        agent_stats = torch.from_numpy(agent_stats).type(torch.FloatTensor).to(device)
+        around_agents = torch.from_numpy(around_agents).type(torch.FloatTensor).to(device).unsqueeze(1)/5991.0
         states = (matrixs,around_agents,agent_stats)
         
-        matrix_ps = torch.from_numpy(matrix_ps).type(torch.LongTensor).to(device)
-        agent_stat_ps = torch.from_numpy(agent_stat_ps).to(device)
-        around_agent_ps = torch.from_numpy(around_agent_ps).type(torch.LongTensor).to(device)
+        matrix_ps = torch.from_numpy(matrix_ps).type(torch.FloatTensor).to(device).unsqueeze(1)/5991.0
+        agent_stat_ps = torch.from_numpy(agent_stat_ps).type(torch.FloatTensor).to(device)
+        around_agent_ps = torch.from_numpy(around_agent_ps).type(torch.FloatTensor).to(device).unsqueeze(1)/5991.0
         next_states = (matrix_ps,around_agent_ps,agent_stat_ps)
-
+        
         prediction = self.dqn(states)
         current_q_value = prediction.gather(1,actions.unsqueeze(1))
 
@@ -337,78 +332,37 @@ class DQNAgent:
         :param state: the current state
         :return: the action to take
         """ 
-        glyphs_matrix = state['glyphs']
-        agent_stat = state['blstats']
-        agent_row = state['blstats'][1]
-        agent_col = state['blstats'][0]
-
-        around_agent = np.zeros([9,9])
-        row = agent_row - 4
-        col = agent_col -4
-        for i in range(9):
-            for j in range(9):
-                if row>0 and row<glyphs_matrix.shape[0] and col>0 and col<glyphs_matrix.shape[1]:
-                    around_agent[i][j] = glyphs_matrix[row][col]
-                col+=1
-            col = agent_col -4
-            row+=1
-            
-        glyphs_matrix = torch.from_numpy(glyphs_matrix).type(torch.LongTensor).to(device).unsqueeze(0)
-        agent_stat = torch.from_numpy(agent_stat).to(device).unsqueeze(0)
-        around_agent = torch.from_numpy(around_agent).type(torch.LongTensor).to(device).unsqueeze(0)
+        glyphs_matrix,around_agent,agent_stat = to_state(state)
+        glyphs_matrix = torch.from_numpy(glyphs_matrix).type(torch.FloatTensor).to(device).unsqueeze(0).unsqueeze(0)/5991.0
+        agent_stat = torch.from_numpy(agent_stat).type(torch.FloatTensor).to(device).unsqueeze(0)
+        around_agent = torch.from_numpy(around_agent).type(torch.FloatTensor).to(device).unsqueeze(0).unsqueeze(0)/5991.0
         state = (glyphs_matrix,around_agent,agent_stat)
-
+     
         with torch.no_grad():
             outputs = self.dqn(state)
             action = torch.argmax(outputs)
         return action.item()
     
-def to_state(state):
-    glyphs_matrix = state['glyphs']
-    agent_stat = state['blstats']
-    agent_row = state['blstats'][1]
-    agent_col = state['blstats'][0]
-
-    around_agent = np.zeros([9,9])
-    row = agent_row - 4
-    col = agent_col -4
-    for i in range(9):
-        for j in range(9):
-            if row>0 and row<glyphs_matrix.shape[0] and col>0 and col<glyphs_matrix.shape[1]:
-                around_agent[i][j] = glyphs_matrix[row][col]
-            col+=1
-        col = agent_col -4
-        row+=1
-        
-    return glyphs_matrix.copy(),around_agent.copy(),agent_stat.copy()
-def action_to_mean(action):
-    action_meaning = np.arange(1,17)
-    action_meaning = np.append(action_meaning,20)
-    action_meaning = np.append(action_meaning,21)
-    action_meaning = np.append(action_meaning,22)
-    return action_meaning[action]
-    
 def train_dqn(env,hyper_params):
     replay_buffer = ReplayBuffer(hyper_params["replay-buffer-size"])
-    agent=DQNAgent(env.observation_space,19,replay_buffer,\
+    agent=DQNAgent(env.observation_space,env.action_space.n,replay_buffer,\
                    hyper_params["learning-rate"],hyper_params["batch-size"],hyper_params["discount-factor"])
 
     eps_timesteps = hyper_params["eps-fraction"] * float(hyper_params["num-steps"])
     episode_rewards = [0.0]
     train_loss = []
     state = env.reset()
-    
     for t in range(hyper_params["num-steps"]):
         fraction = min(1.0, float(t) / eps_timesteps)
         eps_threshold=hyper_params["eps-start"]+fraction*(hyper_params["eps-end"]-hyper_params["eps-start"])
         sample = random.random()
         if sample <= eps_threshold:
-            action = np.random.choice(19)
+            action = np.random.choice(env.action_space.n)
         else:
             action = agent.act(state)
 
         #take step in env
-        state_prime,reward,done,_ = env.step(action_to_mean(action))
+        state_prime,reward,done,_ = env.step(action)
         
         # clip rewards
         reward = np.tanh(reward/100)
@@ -459,18 +413,18 @@ if __name__ == '__main__':
     plt.plot(rewards)
     plt.ylabel("reward")
     plt.xlabel("episode")
-    plt.savefig('img/nethack_reward.png')
+    plt.savefig('img/nethack_dqn_reward.png')
     
     plt.figure(figsize=(15,15))
     plt.plot(mean_rewards)
     plt.ylabel("mean 100 episode reward")
     plt.xlabel("episode")
-    plt.savefig('img/nethack_mean_reward.png')
+    plt.savefig('img/nethack_dqn_mean_reward.png')
     
     plt.figure(figsize=(15,15))
     plt.plot(loss)
     plt.ylabel("loss")
-    plt.savefig('img/nethack_loss.png')
+    plt.savefig('img/nethack_dqn_loss.png')
     
     #Number of times each seed will be run
     num_runs = 10
